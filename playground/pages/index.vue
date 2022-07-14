090730c79d1e973c2834ecadf7ef4ae0ca9ec94b
Tony Rice <wantpinow@Patricks-MacBook-Pro.local>
2022-07-14 15:44:37 +0100

    add manual reset / fetch

--- playground/pages/index.vue
+++ playground/pages/index.vue
@@ -1,3 +1,11 @@
 <template>
-  <div></div>
+  <div>
+    <button @click="resetUserAttributes">Reset manually!</button>
+    <button @click="fetchUserAttributes">Fetch manually!</button>
+  </div>
 </template>
+
+<script setup>
+const { reset: resetUserAttributes, fetch: fetchUserAttributes } =
+  useSupabaseUserAttributes();
+</script>
--- src/runtime/composables/useSupabaseUserAttributes.ts
+++ src/runtime/composables/useSupabaseUserAttributes.ts
@@ -5,7 +5,6 @@ export const useSupabaseUserAttributes = (): any => {
   const tables = config.public.supabaseUserAttributes.tables;
 
   let attributes = {};
-
   for (let i = 0; i < tables.length; i++) {
     const table = tables[i];
     const table_name = table.name;
@@ -14,5 +13,43 @@ export const useSupabaseUserAttributes = (): any => {
       `_supabase_user_attributes_${table_name}`
     );
   }
-  return attributes;
+
+  async function fetch() {
+    const client = useSupabaseClient();
+    const user = useSupabaseUser();
+    if (!user.value?.id) {
+      return;
+    }
+    for (let i = 0; i < tables.length; i++) {
+      const table = tables[i];
+      const table_name = table.name;
+      const table_alias = table.alias ?? table.name;
+      const user_id_column = table.user_id_column ?? "id";
+
+      const { data, error } = await client
+        .from(table_name)
+        .select("*")
+        .eq(user_id_column, user.value.id);
+
+      let value = null;
+      if (!error) {
+        if (table.multiple) {
+          value = data;
+        } else if (data.length == 1) {
+          value = data[0];
+        }
+      }
+      attributes[table_alias].value = value;
+    }
+  }
+
+  async function reset() {
+    for (let i = 0; i < tables.length; i++) {
+      const table = tables[i];
+      const table_alias = table.alias ?? table.name;
+      attributes[table_alias].value = null;
+    }
+  }
+
+  return { fetch, reset, ...attributes };
 };
